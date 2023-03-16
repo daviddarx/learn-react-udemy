568224675d348905849539a34c3b0a7263bc262d
David Darx <david.darx@liip.ch>
2023-03-16 18:50:04 +0100

    feat(setup): add console clear

--- src/App.js
+++ src/App.js
@@ -28,29 +28,15 @@ function App() {
     },
   ];
 
+  console.clear(); // eslint-disable-line no-console
+
   return (
     <div>
       <h2>Let's get started!</h2>
-      <ExpenseItem
-        title={expenses[0].title}
-        amount={expenses[0].amount}
-        date={expenses[0].date}
-      />
-      <ExpenseItem
-        title={expenses[1].title}
-        amount={expenses[1].amount}
-        date={expenses[1].date}
-      />
-      <ExpenseItem
-        title={expenses[2].title}
-        amount={expenses[2].amount}
-        date={expenses[2].date}
-      />
-      <ExpenseItem
-        title={expenses[3].title}
-        amount={expenses[3].amount}
-        date={expenses[3].date}
-      />
+      <ExpenseItem title={expenses[0].title} amount={expenses[0].amount} date={expenses[0].date} />
+      <ExpenseItem title={expenses[1].title} amount={expenses[1].amount} date={expenses[1].date} />
+      <ExpenseItem title={expenses[2].title} amount={expenses[2].amount} date={expenses[2].date} />
+      <ExpenseItem title={expenses[3].title} amount={expenses[3].amount} date={expenses[3].date} />
     </div>
   );
 }
