8d72f5a6a3cc62363a2fdb4ec42e39def1e5b8a4
David Darx <david.darx@liip.ch>
2023-03-17 15:33:22 +0100

    feat(expenses): add filter with two way binding

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,11 +1,21 @@
+import { useState } from 'react';
 import Card from '../UI/Card';
 import ExpenseItem from './ExpenseItem';
+import ExpensesFilter from './ExpensesFilter';
 
 import './Expenses.css';
 
 const Expenses = (props) => {
+  const [filteredYear, setFilteredYear] = useState('2022');
+
+  const filterHandler = (selectedYear) => {
+    setFilteredYear(selectedYear);
+    console.log('Filter update', selectedYear);
+  };
+
   return (
     <Card className='expenses'>
+      <ExpensesFilter selectedYear={filteredYear} onFilter={filterHandler} />
       <ExpenseItem
         title={props.items[0].title}
         amount={props.items[0].amount}
