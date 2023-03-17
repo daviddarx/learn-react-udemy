5af83a66e4f00554bf9e54a0cc4710c05531cdc8
David Darx <david.darx@liip.ch>
2023-03-17 08:28:05 +0100

    feat(new-expense): add new component

---
+++ src/components/NewExpense/NewExpense.js
@@ -0,0 +1,14 @@
+import Card from '../UI/Card';
+import ExpenseForm from './ExpenseForm';
+
+import './NewExpense.css';
+
+const NewExpense = () => {
+  return (
+    <Card className='new-expense'>
+      <ExpenseForm />
+    </Card>
+  );
+};
+
+export default NewExpense;
