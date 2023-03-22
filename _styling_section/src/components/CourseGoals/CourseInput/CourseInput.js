718d9f0c622f96ad2300a610a92f62aa3e3cfcb7
David Darx <david.darx@liip.ch>
2023-03-22 17:53:33 +0100

    feat(course-input): add warning state to input with dynamic inline styling

--- _styling_section/src/components/CourseGoals/CourseInput/CourseInput.js
+++ _styling_section/src/components/CourseGoals/CourseInput/CourseInput.js
@@ -3,25 +3,37 @@ import React, { useState } from 'react';
 import Button from '../../UI/Button/Button';
 import './CourseInput.css';
 
-const CourseInput = props => {
+const CourseInput = (props) => {
   const [enteredValue, setEnteredValue] = useState('');
+  const [isValid, setIsValid] = useState(true);
 
-  const goalInputChangeHandler = event => {
+  const goalInputChangeHandler = (event) => {
     setEnteredValue(event.target.value);
   };
 
-  const formSubmitHandler = event => {
+  const formSubmitHandler = (event) => {
     event.preventDefault();
+    if (enteredValue.trim().length === 0) {
+      setIsValid(false);
+      return;
+    }
     props.onAddGoal(enteredValue);
+    setEnteredValue('');
+    setIsValid(true);
   };
 
   return (
     <form onSubmit={formSubmitHandler}>
-      <div className="form-control">
-        <label>Course Goal</label>
-        <input type="text" onChange={goalInputChangeHandler} />
+      <div className='form-control'>
+        <label style={{ color: isValid ? 'inherit' : 'red' }}>Course Goal</label>
+        <input
+          type='text'
+          value={enteredValue}
+          onChange={goalInputChangeHandler}
+          style={{ borderColor: isValid ? null : 'red' }}
+        />
       </div>
-      <Button type="submit">Add Goal</Button>
+      <Button type='submit'>Add Goal</Button>
     </form>
   );
 };
