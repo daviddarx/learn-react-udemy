df5e51a255bec455ce2344052cc007b873a2321f
David Darx <david.darx@liip.ch>
2023-03-23 19:57:16 +0100

    feat(ui): add card and button components

--- _08_exercise_project/src/components/Users/AddUser.js
+++ _08_exercise_project/src/components/Users/AddUser.js
@@ -1,3 +1,6 @@
+import Card from '../UI/Card';
+import Button from '../UI/Button';
+
 export default function AddUser() {
   const submitUser = (e) => {
     console.log(e.target);
@@ -6,12 +9,16 @@ export default function AddUser() {
   };
 
   return (
-    <form onSubmit={submitUser}>
-      <label htmlFor='username'>User name</label>
-      <input type='text' id='username' />
-      <label htmlFor='age'>Age</label>
-      <input type='number' id='age' />
-      <button type='submit'>Add user</button>
-    </form>
+    <Card>
+      <form onSubmit={submitUser}>
+        <div>
+          <label htmlFor='username'>User name</label>
+          <input type='text' id='username' />
+          <label htmlFor='age'>Age</label>
+          <input type='number' id='age' />
+        </div>
+        <Button type='submit'>Add user</Button>
+      </form>
+    </Card>
   );
 }
