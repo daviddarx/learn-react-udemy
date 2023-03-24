24ae1f1cebfc7b06edb68507050b9f30d91a8182
David Darx <david.darx@liip.ch>
2023-03-24 08:33:54 +0100

    feat(app): use react fragment instead of custom wrapper helper

--- _09_fragments_portals_refs/src/App.js
+++ _09_fragments_portals_refs/src/App.js
@@ -1,6 +1,5 @@
-import React, { useState } from 'react';
+import React, { useState, Fragment } from 'react';
 
-import Wrapper from './components/Helpers/Wrapper';
 import AddUser from './components/Users/AddUser';
 import UsersList from './components/Users/UsersList';
 
@@ -14,10 +13,10 @@ function App() {
   };
 
   return (
-    <Wrapper>
+    <Fragment>
       <AddUser onAddUser={addUserHandler} />
       <UsersList users={usersList} />
-    </Wrapper>
+    </Fragment>
   );
 }
 
