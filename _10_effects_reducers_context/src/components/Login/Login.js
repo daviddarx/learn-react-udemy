4e5e17b20292abf1556e2d7661ac98ec92a76dee
David Darx <david.darx@liip.ch>
2023-03-24 18:15:50 +0100

    feat(login): use useEffect only on the isValid state of password and email.

--- _10_effects_reducers_context/src/components/Login/Login.js
+++ _10_effects_reducers_context/src/components/Login/Login.js
@@ -37,18 +37,27 @@ const Login = (props) => {
     isValid: undefined,
   });
 
+  /**
+   * create variables for emailState.isValid and passWordState.isValid in order
+   * to be able to listen only their change in useEffect, and not the changes of the full objects
+   * emailState and passWordState
+   */
+  const { isValid: isEmailValid } = emailState; // destructuring with alias
+  const { isValid: isPasswordValid } = passwordState;
+
+  console.log('----form valid: ', formIsValid);
+
   useEffect(() => {
     const formValidationTimeout = setTimeout(() => {
-      setFormIsValid(emailState.isValid && passwordState.isValid);
+      setFormIsValid(isEmailValid && isPasswordValid);
+      console.log('TEST FORM: ', isEmailValid && isPasswordValid);
     }, 500);
 
     // return a clean-up function, which will be executed before the next useEffect update.
     return () => {
       clearTimeout(formValidationTimeout);
     };
-  }, [emailState, passwordState]);
-
-  console.log(formIsValid);
+  }, [isEmailValid, isPasswordValid]);
 
   const emailChangeHandler = (event) => {
     dispatchEmail({
