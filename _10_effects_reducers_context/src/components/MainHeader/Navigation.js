b8652c496cfbefcc392a446de74442c2d9f2b4c5
David Darx <david.darx@liip.ch>
2023-03-24 19:42:01 +0100

    feat(navigation): simplify the context listening with useContext() hook

--- _10_effects_reducers_context/src/components/MainHeader/Navigation.js
+++ _10_effects_reducers_context/src/components/MainHeader/Navigation.js
@@ -1,33 +1,28 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useContext } from 'react';
 
 import AuthContext from '../../store/auth-context';
 import classes from './Navigation.module.css';
 
 const Navigation = (props) => {
+  const ctx = useContext(AuthContext);
   return (
-    <AuthContext.Consumer>
-      {(ctx) => {
-        return (
-          <nav className={classes.nav}>
-            <ul>
-              {ctx.isLoggedIn && (
-                <Fragment>
-                  <li>
-                    <a href='/'>Users</a>
-                  </li>
-                  <li>
-                    <a href='/'>Admin</a>
-                  </li>
-                  <li>
-                    <button onClick={props.onLogout}>Logout</button>
-                  </li>
-                </Fragment>
-              )}
-            </ul>
-          </nav>
-        );
-      }}
-    </AuthContext.Consumer>
+    <nav className={classes.nav}>
+      <ul>
+        {ctx.isLoggedIn && (
+          <Fragment>
+            <li>
+              <a href='/'>Users</a>
+            </li>
+            <li>
+              <a href='/'>Admin</a>
+            </li>
+            <li>
+              <button onClick={props.onLogout}>Logout</button>
+            </li>
+          </Fragment>
+        )}
+      </ul>
+    </nav>
   );
 };
 
