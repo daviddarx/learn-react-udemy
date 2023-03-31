3f4ecb4cb258d367e7d6a6d670eb8e8a812dccd1
David Darx <david.darx@liip.ch>
2023-03-31 14:53:28 +0200

    feat(header-cart-button): reduce the amount to get all items amount

--- _11_exercise_project_food_app/src/components/Layout/HeaderCartButton.js
+++ _11_exercise_project_food_app/src/components/Layout/HeaderCartButton.js
@@ -8,13 +8,17 @@ import classes from './HeaderCartButton.module.css';
 const HeaderCartButton = ({ onClick }) => {
   const cartCtx = useContext(CartContext);
 
+  const cartAmount = cartCtx.items.reduce((currrentNumber, item) => {
+    return currrentNumber + item.amount;
+  }, 0);
+
   return (
     <button className={classes.button} onClick={onClick}>
       <span>
         <CartIcon className={classes.icon} />
       </span>
       <span>Your cart</span>
-      <span className={classes.badge}>{cartCtx.items.length}</span>
+      <span className={classes.badge}>{cartAmount}</span>
     </button>
   );
 };
