ef4d26cb6c0d0b103b9a8b2ffbfb35e85c2a6f46
David Darx <david.darx@liip.ch>
2023-04-01 09:40:13 +0200

    feat(app): make tests with react.memo und useCallback

--- _12_behind_the_scene_optimization/src/App.js
+++ _12_behind_the_scene_optimization/src/App.js
@@ -1,11 +1,28 @@
-import React from 'react';
+import React, { useState, useCallback } from 'react';
 
 import './App.css';
+import Button from './components/UI/Button/Button';
+import DemoOutput from './components/Demo/DemoOutput';
 
 function App() {
+  const [isShown, setIsShwown] = useState(false);
+
+  console.log('---');
+  console.log('App evaluated');
+
+  const toggleParagraph = useCallback(() => {
+    setIsShwown((prevState) => {
+      return !prevState;
+    });
+  }, []);
+
   return (
-    <div className="app">
+    <div className='app'>
       <h1>Hi there!</h1>
+      <DemoOutput show={isShown} />
+      {/* won't be evaluated because false is a primitive which doesn't change */}
+      <DemoOutput show={false} />
+      <Button onClick={toggleParagraph}>Test</Button>
     </div>
   );
 }
