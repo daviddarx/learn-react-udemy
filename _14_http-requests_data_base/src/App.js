7281b819b899ab74195ca35df94747c2ea0e87cc
David Darx <david.darx@liip.ch>
2023-04-05 19:08:47 +0200

    feat(app): for the promises, use async/await instead of then()

--- _14_http-requests_data_base/src/App.js
+++ _14_http-requests_data_base/src/App.js
@@ -6,28 +6,26 @@ import './App.css';
 function App() {
   const [movies, setMovies] = useState([]);
 
-  const fetchMovies = () => {
+  async function fetchMovies() {
     /**
      * Fetch return a responve, which we listen with the first then().
      * The response has a built-in .json() method, to transform the json into a javascript object.
      * The .json() method also return a responsive, which we listen with the second then();
      */
-    fetch('https://swapi.dev/api/films/')
-      .then((response) => {
-        return response.json();
-      })
-      .then((data) => {
-        const transformedMovies = data.results.map((item) => {
-          return {
-            id: item.episode_id,
-            title: item.title,
-            openingText: item.opening_crawl,
-            releaseDate: item.release_date,
-          };
-        });
-        setMovies(transformedMovies);
-      });
-  };
+    const response = await fetch('https://swapi.dev/api/films/');
+    const data = await response.json();
+
+    const transformedMovies = data.results.map((item) => {
+      return {
+        id: item.episode_id,
+        title: item.title,
+        openingText: item.opening_crawl,
+        releaseDate: item.release_date,
+      };
+    });
+
+    setMovies(transformedMovies);
+  }
 
   return (
     <React.Fragment>
