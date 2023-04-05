a86c4896f5d09052376be26d6378641a99783b85
David Darx <david.darx@liip.ch>
2023-04-05 19:00:58 +0200

    feat(app): send httprequest with fetch to retrieve data

--- _14_http-requests_data_base/src/App.js
+++ _14_http-requests_data_base/src/App.js
@@ -1,31 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import MoviesList from './components/MoviesList';
 import './App.css';
 
 function App() {
-  const dummyMovies = [
-    {
-      id: 1,
-      title: 'Some Dummy Movie',
-      openingText: 'This is the opening text of the movie',
-      releaseDate: '2021-05-18',
-    },
-    {
-      id: 2,
-      title: 'Some Dummy Movie 2',
-      openingText: 'This is the second opening text of the movie',
-      releaseDate: '2021-05-19',
-    },
-  ];
+  const [movies, setMovies] = useState([]);
+
+  const fetchMovies = () => {
+    /**
+     * Fetch return a responve, which we listen with the first then().
+     * The response has a built-in .json() method, to transform the json into a javascript object.
+     * The .json() method also return a responsive, which we listen with the second then();
+     */
+    fetch('https://swapi.dev/api/films/')
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        const transformedMovies = data.results.map((item) => {
+          return {
+            id: item.episode_id,
+            title: item.title,
+            openingText: item.opening_crawl,
+            releaseDate: item.release_date,
+          };
+        });
+        setMovies(transformedMovies);
+      });
+  };
 
   return (
     <React.Fragment>
       <section>
-        <button>Fetch Movies</button>
+        <button onClick={fetchMovies}>Fetch Movies</button>
       </section>
       <section>
-        <MoviesList movies={dummyMovies} />
+        <MoviesList movies={movies} />
       </section>
     </React.Fragment>
   );
