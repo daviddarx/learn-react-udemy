9ff785bb4abfbee9f74217846b20e0aa60a46803
David Darx <david.darx@liip.ch>
2023-04-07 07:02:12 +0200

    feat(simple-input): add two-way binding and form submition

--- _16_forms/src/components/SimpleInput.js
+++ _16_forms/src/components/SimpleInput.js
@@ -1,11 +1,26 @@
+import { useState } from 'react';
+
 const SimpleInput = (props) => {
+  const [name, setName] = useState('');
+
+  const onChange = (e) => {
+    setName(e.target.value);
+  };
+
+  const submitForm = (e) => {
+    e.preventDefault();
+
+    console.log(name);
+    setName('');
+  };
+
   return (
-    <form>
+    <form onSubmit={submitForm}>
       <div className='form-control'>
         <label htmlFor='name'>Your Name</label>
-        <input type='text' id='name' />
+        <input type='text' id='name' value={name} onChange={onChange} />
       </div>
-      <div className="form-actions">
+      <div className='form-actions'>
         <button>Submit</button>
       </div>
     </form>
