17f4c13c4f8c6cb608f1b36905e76b2407126693
David Darx <david.darx@liip.ch>
2023-04-07 07:12:58 +0200

    feat(simple-input): add validation on submit

--- _16_forms/src/components/SimpleInput.js
+++ _16_forms/src/components/SimpleInput.js
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 
 const SimpleInput = (props) => {
+  const [isNameValid, setNameIsValid] = useState(true);
   const [name, setName] = useState('');
 
   const onChange = (e) => {
@@ -10,15 +11,22 @@ const SimpleInput = (props) => {
   const submitForm = (e) => {
     e.preventDefault();
 
+    if (name.trim().length === 0) {
+      setNameIsValid(false);
+
+      return;
+    }
+
     console.log(name);
     setName('');
   };
 
   return (
     <form onSubmit={submitForm}>
-      <div className='form-control'>
+      <div className={`form-control${!isNameValid ? ' invalid' : ''}`}>
         <label htmlFor='name'>Your Name</label>
         <input type='text' id='name' value={name} onChange={onChange} />
+        {!isNameValid && <p className='error-text'>Name must not be empty</p>}
       </div>
       <div className='form-actions'>
         <button>Submit</button>
