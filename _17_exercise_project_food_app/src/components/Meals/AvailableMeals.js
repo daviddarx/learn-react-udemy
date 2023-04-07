8cf5d41f6b0509af1a18fd3f4af3e627401735cc
David Darx <david.darx@liip.ch>
2023-04-07 13:23:02 +0200

    feat(available-meals): retrieve data from firebase db

--- _17_exercise_project_food_app/src/components/Meals/AvailableMeals.js
+++ _17_exercise_project_food_app/src/components/Meals/AvailableMeals.js
@@ -1,36 +1,35 @@
+import { useState, useEffect, useCallback } from 'react';
+
 import Card from '../UI/Card';
 import MealItem from './MealItem/MealItem';
 import classes from './AvailableMeals.module.css';
 
-const DUMMY_MEALS = [
-  {
-    id: 'm1',
-    name: 'Sushi',
-    description: 'Finest fish and veggies',
-    price: 22.99,
-  },
-  {
-    id: 'm2',
-    name: 'Schnitzel',
-    description: 'A german specialty!',
-    price: 16.5,
-  },
-  {
-    id: 'm3',
-    name: 'Barbecue Burger',
-    description: 'American, raw, meaty',
-    price: 12.99,
-  },
-  {
-    id: 'm4',
-    name: 'Green Bowl',
-    description: 'Healthy...and green...',
-    price: 18.99,
-  },
-];
-
 const AvailableMeals = () => {
-  const mealsList = DUMMY_MEALS.map((meal) => (
+  const [meals, setMeals] = useState([]);
+
+  const retrieveData = useCallback(async () => {
+    const response = await fetch('https://react-http-d10a2-default-rtdb.firebaseio.com/meals.json');
+    const responseData = await response.json();
+
+    const loadedMeals = [];
+
+    /* Firebase data is an object, we need to transform data */
+    for (const key in responseData) {
+      loadedMeals.push({
+        id: responseData[key].id,
+        name: responseData[key].name,
+        description: responseData[key].description,
+        price: responseData[key].price,
+      });
+    }
+    setMeals(loadedMeals);
+  }, []);
+
+  useEffect(() => {
+    retrieveData();
+  }, [retrieveData]);
+
+  const mealsList = meals.map((meal) => (
     <MealItem
       key={meal.id}
       id={meal.id}
