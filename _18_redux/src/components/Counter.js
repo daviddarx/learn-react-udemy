30b05927c07556967c80a9e2f3174ec6b941ceab
David Darx <david.darx@liip.ch>
2023-04-13 20:47:11 +0200

    feat(store): create redux store, provide it and use it in component

--- _18_redux/src/components/Counter.js
+++ _18_redux/src/components/Counter.js
@@ -1,12 +1,38 @@
 import classes from './Counter.module.css';
+import { useSelector, useDispatch } from 'react-redux';
 
 const Counter = () => {
-  const toggleCounterHandler = () => {};
+  /**
+   * Pick the part of the state that we want.
+   * React-redux will automatically set a subscriber for this to get the state updated.
+   */
+  const counter = useSelector((state) => state.counter);
+
+  /**
+   * Retrieve a function to dispatch the actions.
+   */
+  const dispatchFn = useDispatch();
+
+  const incrementHandler = () => {
+    dispatchFn({ type: 'increment' });
+  };
+
+  const decrementHandler = () => {
+    dispatchFn({ type: 'decrement' });
+  };
+
+  const toggleCounterHandler = () => {
+    console.log('counter');
+  };
 
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
-      <div className={classes.value}>-- COUNTER VALUE --</div>
+      <div className={classes.value}>{counter}</div>
+      <div>
+        <button onClick={incrementHandler}>Increment</button>
+        <button onClick={decrementHandler}>Decrement</button>
+      </div>
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
     </main>
   );
