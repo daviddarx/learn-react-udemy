f4a29dff2613b02cb9b84514fa33d3e47efe15f9
David Darx <david.darx@liip.ch>
2023-04-14 14:56:08 +0200

    feat(app): refactor cart db uploading with custom action creator

--- _19_redux_advanced/src/App.js
+++ _19_redux_advanced/src/App.js
@@ -1,7 +1,7 @@
-import { useEffect, useCallback, Fragment } from 'react';
+import { useEffect, Fragment } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 
-import { uiActions } from './store/ui';
+import { sendCartData } from './store/cart';
 
 import Cart from './components/Cart/Cart';
 import Layout from './components/Layout/Layout';
@@ -17,60 +17,14 @@ function App() {
   const cart = useSelector((state) => state.cart);
   const notification = useSelector((state) => state.ui.notification);
 
-  const uploadCart = useCallback(
-    async (cart) => {
-      dispatch(
-        uiActions.showNotification({
-          status: '',
-          title: 'Updating database',
-          message: 'Updating database...',
-        }),
-      );
-
-      try {
-        const response = await fetch(
-          'https://react-http-d10a2-default-rtdb.firebaseio.com/cart.json',
-          {
-            method: 'PUT' /* override existing data */,
-            body: JSON.stringify(cart),
-            headers: {
-              'Content-Type': 'application/json',
-            },
-          },
-        );
-
-        if (!response.ok) {
-          throw new Error('Sending cart data failed.');
-        }
-
-        dispatch(
-          uiActions.showNotification({
-            status: 'success',
-            title: 'Success',
-            message: 'Sending cart data succeed.',
-          }),
-        );
-      } catch (error) {
-        dispatch(
-          uiActions.showNotification({
-            status: 'error',
-            title: 'Error',
-            message: 'Sending cart data failed.',
-          }),
-        );
-      }
-    },
-    [dispatch],
-  );
-
   useEffect(() => {
     if (isInitial) {
       isInitial = false;
       return;
-    } else {
-      uploadCart(cart);
     }
-  }, [cart, uploadCart]);
+
+    dispatch(sendCartData(cart));
+  }, [cart, dispatch]);
 
   return (
     <Fragment>
