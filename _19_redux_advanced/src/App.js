6a07859e60ce1015055498df6957d3aea3e53533
David Darx <david.darx@liip.ch>
2023-04-14 13:44:18 +0200

    feat(app): listen to state change and update database with the new cart

--- _19_redux_advanced/src/App.js
+++ _19_redux_advanced/src/App.js
@@ -1,3 +1,4 @@
+import { useEffect, useCallback } from 'react';
 import { useSelector } from 'react-redux';
 
 import Cart from './components/Cart/Cart';
@@ -6,6 +7,23 @@ import Products from './components/Shop/Products';
 
 function App() {
   const isCartShown = useSelector((state) => state.ui.isCartShown);
+  const cart = useSelector((state) => state.cart);
+
+  const uploadCart = useCallback(async (cart) => {
+    await fetch('https://react-http-d10a2-default-rtdb.firebaseio.com/cart.json', {
+      method: 'PUT' /* override existing data */,
+      body: JSON.stringify(cart),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+  }, []);
+
+  useEffect(() => {
+    if (cart.totalQuantity !== 0) {
+      uploadCart(cart);
+    }
+  }, [cart, uploadCart]);
 
   return (
     <Layout>
