d3e9cab8b013c6958cb5a64626498269bac524eb
David Darx <david.darx@liip.ch>
2023-04-21 10:05:52 +0200

    feat(home): display meetups list

--- _23-1_nextjs_meetup/pages/index.js
+++ _23-1_nextjs_meetup/pages/index.js
@@ -1,5 +1,31 @@
+import { Fragment } from 'react';
+import MeetupList from '../components/meetups/MeetupList';
+
+const DUMMY_MEETUPS = [
+  {
+    id: 'm1',
+    title: 'First Meetup',
+    image:
+      'https://www.zuerich.com/sites/default/files/styles/683_512_focal_scale_crop/public/image/2021/web_zurich_general_key_ZT_21257_1600x900.jpg?',
+    address: 'Zürich',
+    description: 'This is a first meetup',
+  },
+  {
+    id: 'm2',
+    title: 'Second Meetup',
+    image:
+      'https://www.zuerich.com/sites/default/files/styles/683_512_focal_scale_crop/public/image/2021/web_zurich_general_key_ZT_21257_1600x900.jpg?',
+    address: 'Zürich',
+    description: 'This is a second meetup',
+  },
+];
+
 const HomePage = () => {
-  return <h1>HomePage</h1>;
+  return (
+    <Fragment>
+      <MeetupList meetups={DUMMY_MEETUPS}></MeetupList>
+    </Fragment>
+  );
 };
 
 export default HomePage;
