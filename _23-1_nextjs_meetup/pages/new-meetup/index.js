3d5bb3c80df7e2713748a23f65c67667b0782338
David Darx <david.darx@liip.ch>
2023-04-21 10:24:57 +0200

    feat(new-meetup): add new meetup form

---
+++ _23-1_nextjs_meetup/pages/new-meetup/index.js
@@ -0,0 +1,11 @@
+import NewMeetupForm from './../../components/meetups/NewMeetupForm';
+
+const NewMeetupPage = () => {
+  const onAddMeetupHandler = (meetupData) => {
+    console.log(meetupData);
+  };
+
+  return <NewMeetupForm onAddMeetup={onAddMeetupHandler} />;
+};
+
+export default NewMeetupPage;
