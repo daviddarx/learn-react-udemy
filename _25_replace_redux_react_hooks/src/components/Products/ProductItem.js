620afe7564dbc78f0d4d6b78e422916546620bb8
David Darx <david.darx@liip.ch>
2023-04-21 18:18:22 +0200

    feat(setup): replace redux with custom context from react Context API

--- _25_replace_redux_react_hooks/src/components/Products/ProductItem.js
+++ _25_replace_redux_react_hooks/src/components/Products/ProductItem.js
@@ -1,25 +1,21 @@
-import React from 'react';
-import { useDispatch } from 'react-redux';
+import React, { useContext } from 'react';
 
 import Card from '../UI/Card';
 import './ProductItem.css';
-import { toggleFav } from '../../store/actions/products';
 
-const ProductItem = props => {
-  const dispatch = useDispatch();
+import { ProductsContext } from '../../context/procuts-context';
 
-  const toggleFavHandler = () => {
-    dispatch(toggleFav(props.id));
-  };
+const ProductItem = (props) => {
+  const toggleFav = useContext(ProductsContext).toggleFav;
 
   return (
     <Card style={{ marginBottom: '1rem' }}>
-      <div className="product-item">
+      <div className='product-item'>
         <h2 className={props.isFav ? 'is-fav' : ''}>{props.title}</h2>
         <p>{props.description}</p>
         <button
           className={!props.isFav ? 'button-outline' : ''}
-          onClick={toggleFavHandler}
+          onClick={toggleFav.bind(null, props.id)}
         >
           {props.isFav ? 'Un-Favorite' : 'Favorite'}
         </button>
