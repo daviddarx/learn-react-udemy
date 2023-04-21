1411449853cbbeadc411b753e5254a1aaa0d4ecd
David Darx <david.darx@liip.ch>
2023-04-21 15:51:46 +0200

    feat(api): add an API route to enter new meetup data in a MongoDB

--- _23-1_nextjs_meetup/pages/new-meetup/index.js
+++ _23-1_nextjs_meetup/pages/new-meetup/index.js
@@ -1,8 +1,25 @@
+import { useRouter } from 'next/router';
+
 import NewMeetupForm from './../../components/meetups/NewMeetupForm';
 
 const NewMeetupPage = () => {
-  const onAddMeetupHandler = (meetupData) => {
+  const router = useRouter();
+
+  const onAddMeetupHandler = async (meetupData) => {
     console.log(meetupData);
+    const response = await fetch('/api/new-meetup', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(meetupData),
+    });
+
+    const data = await response.json();
+
+    console.log(data);
+
+    router.push('/');
   };
 
   return <NewMeetupForm onAddMeetup={onAddMeetupHandler} />;
