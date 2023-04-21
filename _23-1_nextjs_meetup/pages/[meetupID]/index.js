58dfe34c3cb5cf2443647aa8cf6de9484b7fd5a2
David Darx <david.darx@liip.ch>
2023-04-21 15:07:36 +0200

    feat(meetup-detail): add static content generation with getStaticProps() and getStaticPaths()

--- _23-1_nextjs_meetup/pages/[meetupID]/index.js
+++ _23-1_nextjs_meetup/pages/[meetupID]/index.js
@@ -1,19 +1,58 @@
-import { useRouter } from 'next/router';
-
 import MeetupDetail from './../../components/meetups/MeetupDetail';
 
-const MeetupDetailPage = () => {
-  const router = useRouter();
-
+const MeetupDetailPage = (props) => {
   return (
     <MeetupDetail
-      id={router.query.meetupID}
-      title='Title'
-      address='Zurich'
-      description='Meetup description'
-      image='https://www.zuerich.com/sites/default/files/styles/683_512_focal_scale_crop/public/image/2021/web_zurich_general_key_ZT_21257_1600x900.jpg'
+      id={props.id}
+      title={props.title}
+      address={props.address}
+      description={props.description}
+      image={props.image}
     />
   );
 };
+
+/**
+ * Export of getStaticPaths() is need in all dynamic page components
+ * which are using getStaticProps. So that NextJS can export/render all the
+ * versions of this page. We have to export all the possible used IDs
+ */
+export async function getStaticPaths() {
+  return {
+    // list of all pathes
+    paths: [
+      {
+        params: { meetupID: 'm1' },
+      },
+      {
+        params: { meetupID: 'm2' },
+      },
+    ],
+    /**
+     * The fallback indicate if the paths[] contains all the possible id's (false).
+     * If true, NextJS will pregenerating the missing ones dynamically when the requests arrive.
+     * Very good if we don't want to generate 100 pages, but only the most popular.
+     */
+    fallback: false,
+  };
+}
+
+export function getStaticProps(context) {
+  const meetupID = context.params.meetupID;
+
+  console.log(meetupID);
+
+  return {
+    props: {
+      id: meetupID,
+      title: 'Title',
+      address: 'Zurich',
+      description: 'Meetup description',
+      image:
+        'https://www.zuerich.com/sites/default/files/styles/683_512_focal_scale_crop/public/image/2021/web_zurich_general_key_ZT_21257_1600x900.jpg',
+    },
+    revalidate: 10,
+  };
+}
 
 export default MeetupDetailPage;
